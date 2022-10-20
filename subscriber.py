b4406ee90199ef59da5ccf513e97440206f490fe
J0emo <josephtseng00@gmail.com>
2022-10-19 17:39:56 -0700

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -6,20 +6,20 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ece180d/test", qos=1)
+    client.subscribe("ece180d/test2", qos=1)
     
 # The callback of the client when it disconnects.
 def on_disconnect(client, userdata, rc):
     if rc != 0:
-    print(’Unexpected Disconnect’)
+        print('Unexpected Disconnect')
     else:
-    print(’Expected Disconnect’)
+        print('Expected Disconnect')
 
 # The default message callback.
 # (you can create separate callbacks per subscribed topic)
 def on_message(client, userdata, message):
-    print(’Received message: "’ + str(message.payload) + ’" on topic "’ +
-        message.topic + ’" with QoS ’ + str(message.qos))
+    print('Received message: "' + str(message.payload) + '" on topic "' +
+        message.topic + '" with QoS ' + str(message.qos))
 
 # 1. create a client instance.
 client = mqtt.Client()
@@ -30,7 +30,7 @@ client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_message = on_message
 # 2. connect to a broker using one of the connect*() functions.
-client.connect_async(’test.mosquitto.org’)
+client.connect_async('test.mosquitto.org')
 # client.connect("mqtt.eclipse.org")
 # 3. call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
